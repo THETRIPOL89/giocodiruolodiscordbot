6a86bedffd2b1e9c309c3b118d332343261baeaf
Federico Il Figo <radio008virtual@gmail.com>
2021-02-03 18:56:32 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,4 +2,8 @@ import discord
 
 client = commands.Bot(command_prefix='&')
 
+@client.event
+async def on_ready():
+    print("The bot is ready.")
+
 client.run("ODA2NTgyMjMwODUxMTI1Mjkx.YBriPQ.iZhDsrK2fRzSEkmL4rM30gi4p8g")
