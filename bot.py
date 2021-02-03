4041ce8ae01314c00bd2e14fef4fd8e16f8be7cc
Federico Il Figo <radio008virtual@gmail.com>
2021-02-03 19:11:12 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,8 +2,14 @@ import discord
 
 client = commands.Bot(command_prefix='&')
 
+timeout =  36000
+
 @client.event
 async def on_ready():
     print("The bot is ready.")
+    channel = client.get_channel(706775759615950908)
+    while True:
+        await channel.send("!add-money-role cash <@805495893766963201> 100")
+        await asyncio.sleep(timeout)
 
 client.run("ODA2NTgyMjMwODUxMTI1Mjkx.YBriPQ.iZhDsrK2fRzSEkmL4rM30gi4p8g")
